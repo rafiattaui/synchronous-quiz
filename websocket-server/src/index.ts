46ba950b7eed5442cd4e9d24cabdda3955ccce02
rafiattaui <athallah.rafi13@gmail.com>
2026-02-19 13:40:04 +0700

    initial commit

---
+++ websocket-server/src/index.ts
@@ -0,0 +1,19 @@
+import { Server } from "socket.io";
+import express, { request, response } from "express";
+import type { Express } from "express";
+import { createServer } from "http";
+import { dirname, join } from "path";
+import type * as q from "./quizinterface.ts";
+
+const app: Express = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+httpServer.listen(3001, () => {
+    console.log("listening on *:3001");
+});
+
---
+++ websocket-server/src/quizinterface.ts
@@ -0,0 +1,36 @@
+export interface Player {
+    userId: string;
+    isOnline: boolean;
+    name: string;
+    score: number;
+    selectedAnswer: number | null; // index of the selected answer, or null if not answered yet
+    answerTime: number | null; // timestamp in ms when the player submitted their answer, or null if not answered yet
+}
+
+export interface Question {
+    questionId: string;
+    question: string;
+    options: string[];
+    correctAnswer: number;
+}
+
+enum QuizStateEnum {
+    LOBBY = "LOBBY",
+    PREQUESTION_COUNTDOWN = "PREQUESTION_COUNTDOWN",
+    QUESTION_REVEALED = "QUESTION_REVEALED",
+    QUESTION_ANSWERING = "QUESTION_ANSWERING",
+    ANSWER_REVEALED = "ANSWER_REVEALED",
+    LEADERBOARD = "LEADERBOARD",
+    FINISHED = "FINISHED",
+}
+
+export interface QuizState {
+    quizId: string;
+    players: Record<string, Player>; // userId -> Player
+    socketIdToUserId: Record<string, string>; // wsId -> userId
+    currentQuestion: Omit<Question, "correctAnswer"> | null;
+    state: QuizStateEnum;
+    phaseEndsAt: number | null; // timestamp in ms when the current phase ends for countdowns,
+    //  or null if not applicable
+}
+
